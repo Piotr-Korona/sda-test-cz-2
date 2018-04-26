80f799add69e8efe825e34458a96d5c3037e8681
Piotr Korona <piotr.korona@tlen.pl>
2018-04-26 18:50:10 +0200

    commit init

--- kody z 26.4.2018 ćwiczenie GITa/src/test/git/piotrkorona/Main.java
+++ kody z 26.4.2018 ćwiczenie GITa/src/test/git/piotrkorona/Main.java
@@ -3,5 +3,6 @@ package test.git.piotrkorona;
 public class Main {
     public static void main (String[] args) {
         System.out.println("Commit zainicjowany");
+        System.out.println("Kolejna zmiana");
     }
 }
